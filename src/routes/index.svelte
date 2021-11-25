8be386e25f91fc6314142409ac2c4ca48803cf4b
ctibo <ctibo@users.noreply.github.com>
2021-11-25 06:22:34 +0000

    deploy: 01ba48d0751d9e66718616f7a45f5a4b8fe7d1d1

---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Banner from '../components/blocks/Banner.svelte';
+</script>
+
+<style lang="scss">
+  @import '../styles/variables';
+
+</style>
+
+<main>
+  <Banner>
+    <h1 class="banner-title">
+      $DEGEN
+    </h1>
+  </Banner>
+
+  <section class="content container">
+    <h2 class="section-title">
+      DEGEN is a governance token.
+    </h2>
+    <p class="big-text">
+      100% of Degen will go into the Tinyman liquidity pool and no Degen will be withheld for the team. 
+      In fact, there is no predetermined 'team', all Degen token holders will have a say in determining the direction of Degen. 
+      We are bootstrapping a community, Degen Token is our governance.
+    </p>
+  </section>
+</main>
